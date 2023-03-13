b312e785abe2a37a2b36f08abb0ee1b08c359c5b
NancyHuaerLi <lhe.nancy1998@gmail.com>
2023-03-13 20:43:08 +0800

    test with pokemon keywords

--- pagerank.py
+++ pagerank.py
@@ -1,123 +1,40 @@
+from py4j.protocol import Py4JJavaError
+from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession, SQLContext
 from pyspark.sql.functions import explode, col, udf, concat_ws, from_json, lit, array, expr, size
 from pyspark.sql.functions import sum as _sum
+from pyspark.sql.types import *
 import json
+import os
 from pyspark.sql.types import BooleanType
+import sys
+print(sys.version)
+conf = SparkConf()
+sc = SparkContext(conf=conf)
+sc.setLogLevel("ERROR")
 
-spark = SparkSession.builder.appName("twitter_applications").getOrCreate()
-# filter based on the following keywords
-test_list = ['russia', 'ukraine', 'putin', 'zelensky'
-                                           'russian', 'ukrainian', 'keiv', 'kyiv']
-# regex for filter
-regex_pattern = "(?i)" + "|".join(test_list)
-
-raw = spark.read.json("./sample_data/*.json", allowBackslashEscapingAnyCharacter=True)
-# twitter json counts
-# print(raw.count())
-'''
-    Step 1:
-    Save all tweets, include original tweets in the dataset and retweeted / quoted tweets
-
-    only select necessary columns
-    can refer to twitter API for better understanding:
-    https://developer.twitter.com/en/docs/twitter-api/v1/data-dictionary/object-model/tweet
-'''
-
-twitter_df = raw.select("created_at", "id_str", col("user.id_str").alias("user_id_str"),
-                        "in_reply_to_status_id_str", "in_reply_to_user_id_str",
-                        "retweeted_status",
-                        "quoted_status",
-                        "text", col("entities.hashtags.text").alias("hashtags"),
-                        col("quoted_status.created_at").alias("quoted_time"),
-                        col("quoted_status.id_str").alias("quoted_original_tweet_id_str"),
-                        col("quoted_status.user.id_str").alias("quoted_original_user_id_str"),
-                        col("retweeted_status.created_at").alias("retweeted_time"),
-                        col("retweeted_status.id_str").alias("retweeted_original_tweet_id_str"),
-                        col("retweeted_status.user.id_str").alias("retweeted_original_user_id_str")
-                        )
-
-# add all retweeted / quoted original tweet themselves to the twitter df
-retweet_rdd = twitter_df.filter(twitter_df.retweeted_status.isNotNull()).select('retweeted_status.*')
-quoted_rdd = twitter_df.filter(twitter_df.quoted_status.isNotNull()).select('quoted_status.*')
-
-# select columns
-retweet_rdd = retweet_rdd.select("created_at", "id_str",
-                                 col("user.id_str").alias("user_id_str"),
-                                 "in_reply_to_status_id_str", "in_reply_to_user_id_str",
-                                 # "quoted_status",
-                                 "text",
-                                 col("entities.hashtags.text").alias("hashtags"),
-                                 col("quoted_status.created_at").alias("quoted_time"),
-                                 col("quoted_status.id_str").alias("quoted_original_tweet_id_str"),
-                                 col("quoted_status.user.id_str").alias("quoted_original_user_id_str"),
-                                 )
-
-quoted_rdd = quoted_rdd.select("created_at", "id_str", col("user.id_str").alias("user_id_str"),
-                               "in_reply_to_status_id_str", "in_reply_to_user_id_str",
-                               "text", col("entities.hashtags.text").alias("hashtags")
-                               )
-twitter_df = twitter_df.drop("retweeted_status", "quoted_status")
-
-# add missing columns to make sure all columns match in the above 3 DFs
-for c in retweet_rdd.columns:
-    if c not in twitter_df.columns:
-        twitter_df = twitter_df.withColumn(c, lit(None))
-    if c not in quoted_rdd.columns:
-        quoted_rdd = quoted_rdd.withColumn(c, lit(None))
-for c in quoted_rdd.columns:
-    if c not in twitter_df.columns:
-        twitter_df = twitter_df.withColumn(c, lit(None))
-    if c not in retweet_rdd.columns:
-        retweet_rdd = retweet_rdd.withColumn(c, lit(None))
-
-for c in twitter_df.columns:
-    if c not in quoted_rdd.columns:
-        quoted_rdd = quoted_rdd.withColumn(c, lit(None))
-    if c not in retweet_rdd.columns:
-        retweet_rdd = retweet_rdd.withColumn(c, lit(None))
-
-# final twitter DFs
-combined_raw = twitter_df.unionByName(retweet_rdd).unionByName(quoted_rdd)
-
-# convert hashtag column (array type) to str for regex expression filter
-raw_df = combined_raw.withColumn("text_hashtag", concat_ws(",", col("hashtags")))
-
-filter_df = raw_df.filter(raw_df.text.rlike(regex_pattern) | raw_df.text_hashtag.rlike(regex_pattern)).distinct()
-print("# of tweets analyzed: " + str(filter_df.count()))
-
-'''
-    Step 2:
-    Find all interacted (reply, retweet, quote) users.
-    if a user A retweet user B's tweet, there will be a directional edge between A and B, A -> B.
-'''
-
-filter_df = filter_df.withColumn("connected_user",
-                                 array(filter_df.quoted_original_user_id_str, filter_df.in_reply_to_user_id_str,
-                                       filter_df.retweeted_original_user_id_str))
-filter_df = filter_df.withColumn("connected_user_clean", expr('filter(connected_user, x -> x is not null)')).drop(
-    "connected_user")
-
-rdd = filter_df.filter(size(filter_df.connected_user_clean) > 0)
-# each line make sure have only one user and one connected user
-rdd = filter_df.withColumn("connected_user_single", explode(filter_df.connected_user_clean)).rdd
-
-# for each user how many times retweet/replay/quote other tweets
-user_interact_rdd = rdd.map(lambda x: (x[2], 1))
-user_interact_rdd = user_interact_rdd.reduceByKey(lambda a, b: a + b)
-
-# each pair of users (A, B), how many times A retweet this person B
-user_pair_rdd = rdd.map(lambda x: ((x[2], x[15]), 1)).reduceByKey(lambda a, b: a + b)
-user_pair_rdd = user_pair_rdd.map(lambda x: (x[0][0], (x[0][1], x[1])))
+spark = SparkSession.builder.appName("twitter_applications")\
+    .config("spark.sql.files.ignoreCorruptFiles", "true")\
+    .config('spark.executor.memory', '45G')\
+    .config('spark.driver.memory', '80G')\
+    .config('spark.driver.maxResultSize', '10G')\
+    .getOrCreate()
 
 '''
     Step 3:
     Implement basic PageRank as described in 
     http://www.diva-portal.org/smash/get/diva2:1104337/FULLTEXT01.pdf Section 3.4.1
 '''
-
+user_pair_rdd = spark.read \
+     .csv("./user_pair/*.csv").rdd
+user_pair_rdd = user_pair_rdd.map(lambda x: (x[0], (x[1], x[2])))
+user_interact_rdd = spark.read \
+     .csv("./user_interact/*.csv").rdd
+id_name = spark.read.option("header",True).csv("./id_name_dict/*.csv")
+id_name.show()
 # if user A retweet B, the from_user = A, to_user = B
 columns = ['from_user', 'to_user', 'retweet_ratio']
-edge_rdd = user_pair_rdd.join(user_interact_rdd).map(lambda x: (x[0], x[1][0][0], x[1][0][1] / x[1][1])) \
+edge_rdd = user_pair_rdd.join(user_interact_rdd).map(lambda x: (x[0], x[1][0][0], float(x[1][0][1]) / float(x[1][1]))) \
     .filter(lambda x: x[0] != x[1]).toDF(columns)  # remove all self pointed edges
 
 all_users = edge_rdd.select("from_user").union(edge_rdd.select("to_user")).distinct() \
@@ -126,18 +43,24 @@ all_users = edge_rdd.select("from_user").union(edge_rdd.select("to_user")).disti
 N = all_users.count()
 print("number of users in the graph: " + str(N))
 all_users = all_users.withColumn("rank", lit(1 / N))
+damping_factor = 0.85
 
 # keep update the score until convergence
 for cycle in range(10):
     temp = all_users.join(edge_rdd, all_users.users == edge_rdd.to_user, "left").na.fill(value=0,
                                                                                          subset=["retweet_ratio"])
     temp = temp.withColumn("contri", temp.retweet_ratio * temp.rank)
-    all_users = temp.groupby("users").agg(_sum("contri").alias("rank"))
+    all_users = temp.groupby("users").agg(_sum("contri").alias("rank")) #*lit(damping_factor) + lit((1 - damping_factor) / N)
+    all_users = all_users.withColumn("rank", all_users.rank * damping_factor + (1-damping_factor) /N)
     norm = all_users.rdd.map(lambda x: (1, x[1])).reduceByKey(lambda a, b: a + b).collect()[0][1]
     all_users = all_users.withColumn("rank", all_users.rank / norm)
-    all_users.sort(col("rank").desc()).show(5)
+    # all_users.sort(col("rank").desc()).show(5)
+
+results = all_users.sort(col("rank").desc()).limit(10).join(id_name, id_name.user_id_str == all_users.users, "left") \
+    .select("users", "rank", "user_twitter_handle")
+results.show(50)
 
 # write to CSV
-all_users.sort(col("rank").desc()).limit(10).write.csv("./sample_top10")
+results.sort(col("rank").desc()).limit(50).write.mode('overwrite').csv("./test_pokemon_damping")
 
 spark.stop()
---
+++ tweet_filter.py
@@ -0,0 +1,179 @@
+from py4j.protocol import Py4JJavaError
+from pyspark import SparkConf, SparkContext
+import pyspark
+from pyspark.sql import SparkSession, SQLContext
+from pyspark.sql.functions import explode, col, udf, concat_ws, from_json, lit, array, expr, size
+from pyspark.sql.functions import sum as _sum
+from pyspark.sql.types import *
+import json
+import os
+import gc
+from pyspark.sql.types import BooleanType
+
+conf = SparkConf()
+sc = SparkContext(conf=conf)
+sc.setLogLevel("ERROR")
+
+spark = SparkSession.builder.appName("twitter_applications")\
+    .config("spark.sql.files.ignoreCorruptFiles", "true").getOrCreate()
+
+# filter based on the following keywords
+# test_list = ['russia', 'ukraine', 'putin', 'zelensky'
+#                                            'russian', 'ukrainian', 'keiv', 'kyiv']
+test_list = ['nintendo', 'pokemon', 'video game', 'game', 'pokémon legends: arceus', 'pokémon', 'legend of arceus',
+             'legend arceus', 'legends of arceus', 'pokemon legends: arceus', 'stream', 'twitch', 'arceus']
+# regex for filter
+regex_pattern = "(?i)" + "|".join(test_list)
+regex_pattern_hashtag = "(?i)" + "|".join([x.strip(' ') for x in test_list])
+
+
+# raw = spark.read.json("./sample_data/*.json", allowBackslashEscapingAnyCharacter=True)
+# raw = spark.read.json("./202202*/*.json", allowBackslashEscapingAnyCharacter=True)
+def pre_process(folder):
+    raw = spark.read.json(folder+"/*.json.gz", allowBackslashEscapingAnyCharacter=True)
+    # twitter json counts
+    # print(raw.count())
+    '''
+        Step 1:
+        Save all tweets, include original tweets in the dataset and retweeted / quoted tweets
+
+        only select necessary columns
+        can refer to twitter API for better understanding:
+        https://developer.twitter.com/en/docs/twitter-api/v1/data-dictionary/object-model/tweet
+    '''
+
+    twitter_df = raw.select("created_at", "id_str", col("user.id_str").alias("user_id_str"),
+                            col("user.screen_name").alias("user_twitter_handle"),
+                            "in_reply_to_status_id_str", "in_reply_to_user_id_str", "in_reply_to_screen_name",
+                            "retweeted_status",
+                            "quoted_status",
+                            "text", col("entities.hashtags.text").alias("hashtags"),
+                            col("quoted_status.created_at").alias("quoted_time"),
+                            col("quoted_status.id_str").alias("quoted_original_tweet_id_str"),
+                            col("quoted_status.user.id_str").alias("quoted_original_user_id_str"),
+                            col("quoted_status.user.screen_name").alias("quoted_original_user_twitter_handle"),
+                            col("retweeted_status.created_at").alias("retweeted_time"),
+                            col("retweeted_status.id_str").alias("retweeted_original_tweet_id_str"),
+                            col("retweeted_status.user.screen_name").alias("retweeted_original_user_twitter_handle"),
+                            col("retweeted_status.user.id_str").alias("retweeted_original_user_id_str")
+                            )
+
+    # add all retweeted / quoted original tweet themselves to the twitter df
+    retweet_rdd = twitter_df.filter(twitter_df.retweeted_status.isNotNull()).select('retweeted_status.*')
+    quoted_rdd = twitter_df.filter(twitter_df.quoted_status.isNotNull()).select('quoted_status.*')
+
+    # select columns
+    retweet_rdd = retweet_rdd.select("created_at", "id_str",
+                                     col("user.id_str").alias("user_id_str"),
+                                     col("user.screen_name").alias("user_twitter_handle"),
+                                     "in_reply_to_status_id_str", "in_reply_to_user_id_str", "in_reply_to_screen_name",
+                                     # "quoted_status",
+                                     "text",
+                                     col("entities.hashtags.text").alias("hashtags"),
+                                     col("quoted_status.created_at").alias("quoted_time"),
+                                     col("quoted_status.id_str").alias("quoted_original_tweet_id_str"),
+                                     col("quoted_status.user.id_str").alias("quoted_original_user_id_str"),
+                                     col("quoted_status.user.screen_name").alias("quoted_original_user_twitter_handle")
+                                     )
+
+    quoted_rdd = quoted_rdd.select("created_at", "id_str", col("user.id_str").alias("user_id_str"),
+                                   "in_reply_to_status_id_str", "in_reply_to_user_id_str", "in_reply_to_screen_name",
+                                   "text", col("entities.hashtags.text").alias("hashtags")
+                                   )
+    twitter_df = twitter_df.drop("retweeted_status", "quoted_status")
+
+    # add missing columns to make sure all columns match in the above 3 DFs
+    for c in retweet_rdd.columns:
+        if c not in twitter_df.columns:
+            twitter_df = twitter_df.withColumn(c, lit(None))
+        if c not in quoted_rdd.columns:
+            quoted_rdd = quoted_rdd.withColumn(c, lit(None))
+    for c in quoted_rdd.columns:
+        if c not in twitter_df.columns:
+            twitter_df = twitter_df.withColumn(c, lit(None))
+        if c not in retweet_rdd.columns:
+            retweet_rdd = retweet_rdd.withColumn(c, lit(None))
+
+    for c in twitter_df.columns:
+        if c not in quoted_rdd.columns:
+            quoted_rdd = quoted_rdd.withColumn(c, lit(None))
+        if c not in retweet_rdd.columns:
+            retweet_rdd = retweet_rdd.withColumn(c, lit(None))
+
+    # final twitter DFs
+    combined_raw = twitter_df.unionByName(retweet_rdd).unionByName(quoted_rdd)
+    del retweet_rdd
+    del quoted_rdd
+    del twitter_df
+    gc.collect()
+
+    # convert hashtag column (array type) to str for regex expression filter
+    raw_df = combined_raw.withColumn("text_hashtag", concat_ws(",", col("hashtags")))
+
+    filter_df = raw_df.filter(raw_df.text.rlike(regex_pattern) | raw_df.text_hashtag.rlike(regex_pattern)).distinct()
+    del raw_df
+    del combined_raw
+    gc.collect()
+    print("date " + folder[2:] + " # of tweets analyzed: " + str(filter_df.count()))
+    return filter_df
+
+
+cnt = 0
+filter_df = None
+for folder in [x[0] for x in os.walk("./")]:
+    if folder[0:7] == './20220':
+        try:
+            partial_df = pre_process(folder)
+            if cnt == 0:
+                filter_df = partial_df
+            else:
+                filter_df = filter_df.unionByName(partial_df)
+            del partial_df
+            gc.collect()
+        except Py4JJavaError:
+            print(folder+" failed")
+        cnt += 1
+
+# id_name = filter_df.select('user_id_str', 'user_twitter_handle') \
+#     .union(filter_df.select('in_reply_to_user_id_str', 'in_reply_to_screen_name')).na.drop().distinct()
+# id_name.write.option("header", True).mode('overwrite').csv('id_name_dict')
+# print('id name done')
+# del id_name
+# gc.collect()
+
+filter_df = filter_df.drop("user_twitter_handle", "in_reply_to_screen_name",
+                           "quoted_original_user_twitter_handle", "retweeted_original_user_twitter_handle")
+
+'''
+    Step 2:
+    Find all interacted (reply, retweet, quote) users.
+    if a user A retweet user B's tweet, there will be a directional edge between A and B, A -> B.
+'''
+
+filter_df = filter_df.withColumn("connected_user",
+                                 array(filter_df.quoted_original_user_id_str, filter_df.in_reply_to_user_id_str,
+                                       filter_df.retweeted_original_user_id_str))
+filter_df = filter_df.withColumn("connected_user_clean", expr('filter(connected_user, x -> x is not null)')).drop(
+    "connected_user")
+
+
+filter_df = filter_df.filter(size(filter_df.connected_user_clean) > 0)
+
+# each line make sure have only one user and one connected user
+rdd = filter_df.withColumn("connected_user_single", explode(filter_df.connected_user_clean)).rdd
+# rdd.toDF().write.mode('overwrite').csv('intermediate_rdd')
+# del filter_df
+# gc.collect()
+
+# for each user how many times retweet/replay/quote other tweets
+user_interact_rdd = rdd.map(lambda x: (x[2], 1))
+user_interact_rdd = user_interact_rdd.reduceByKey(lambda a, b: a + b)
+
+# each pair of users (A, B), how many times A retweet this person B
+user_pair_rdd = rdd.map(lambda x: ((x[2], x[15]), 1)).reduceByKey(lambda a, b: a + b)
+user_pair_rdd = user_pair_rdd.map(lambda x: (x[0][0], x[0][1], x[1]))
+
+user_pair_rdd.toDF().write.mode('overwrite').csv('user_pair') #coalesce(1, shuffle = True).
+user_interact_rdd.toDF().write.mode('overwrite').csv('user_interact') #coalesce(1, shuffle = True).
+
+spark.stop()
